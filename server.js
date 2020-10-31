85de7b3c6e021f94a06f2285b0a7fe827a7fedc9
henridupon <henridupon@gmail.com>
2020-10-31 19:00:09 +0100

    Added automatic deployment and changed the port.

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.get("/profile", (req, res) => {
   });
 });
 
-const server = app.listen(7000, () => {
+const server = app.listen(3000, () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
